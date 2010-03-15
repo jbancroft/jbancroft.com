a4bb7b3759539bed189df422d37756cbc58b638a
James Bancroft <jim.bancroft@gmail.com>
2010-03-15 14:32:37 -0400

    Made the necessary modifications to get the Cucumber features passing in the previous setup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   Clearance::Routes.draw(map)
   # The priority is based upon order of creation: first created -> highest priority.
+  map.root :controller => 'clearance/sessions', :action => 'new'
 
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
